8895479f95828c671635326f897d9f4b051fd1c1
Isabella Onate <isabellaonate@gmail.com>
2019-01-29 15:04:40 -0600

    player 2 has movement, different parameters and planets. Fixing the flipped image of edmund's planet and Farah's jump.

--- src-code/main.js
+++ src-code/main.js
@@ -26,13 +26,18 @@ let images = {
   alien: "../images/alien.png",
   mannPlanet: "../images/mann-planet-pixel.png",
   mannFloor: "../images/manns-floor-pixel.gif",
-  mannPlanetFlip: "../images/mann-planet-pixel-flip.png"
+  mannPlanetFlip: "../images/mann-planet-pixel-flip.png",
+  edmundPlanet: "../images/edmunds-planet-pixel.gif",
+  edmundPlanetFlip: "../images/edmunds-planet-pixel-flip.gif",
+  edmundFloor: "../images/edmunds-planet-floor-pixel.gif",
 }
 let sounds = {
 
 }
 let gravityMann = .28
-let friction = .8
+let gravityEdmund = .80
+let frictionMann = .3
+let frictionEdmund = .8
 let keys = {}
 
 
@@ -48,12 +53,112 @@ let enemiesP2 = []
 
 // CLASSES
 
+class FloorP1 {
+  constructor () {
+    this.x = 0
+    this.y = 302
+    this.width = canvasP1.width
+    this.height = 50
+    this.image = new Image()
+    this.image.src = images.mannFloor
+    this.image.onload = this.draw()
+  }
+  
+    draw() {
+      ctxP1.drawImage(this.image, this.x, this.y, this.width, this.height)
+    }
+  }
+  
+class FloorP2 {
+  constructor () {
+    this.x = 0
+    this.y = 302
+    this.width = canvasP1.width
+    this.height = 50
+    this.image = new Image()
+    this.image.src = images.edmundFloor
+    this.image.onload = this.draw()
+  }
+  
+    draw() {
+      ctxP2.drawImage(this.image, this.x, this.y, this.width, this.height)
+    }
+  }
+
+  class Planet1 {
+    constructor () {
+      this.x = 0
+      this.y = 0
+      this.width = canvasP1.width
+      this.height = canvasP1.height
+      this.image = new Image()
+      this.image.src = images.mannPlanet
+      // this.image.onload = this.draw()
+      this.image2 = new Image()
+      this.image2.src = images.mannPlanetFlip
+      this.image.onload = this.draw()
+    }
+  
+    draw () {
+      ctxP1.drawImage(this.image, this.x, this.y, this.width, this.height)    
+      ctxP1.drawImage(this.image2, this.x - this.width, this.y, this.width, this.height)
+      ctxP1.drawImage(this.image2, this.x + this.width, this.y, this.width, this.height)
+  
+      if (this.x < -canvasP1.width) {
+        this.x = 0
+      } 
+      
+      if (keys[65]) {
+        this.x++
+        
+      }
+      if (keys[68]) {
+        this.x--
+      }
+  
+    }
+  }
+
+  class Planet2 {
+    constructor () {
+      this.x = 0
+      this.y = 0
+      this.width = canvasP2.width
+      this.height = canvasP2.height
+      this.imageP2 = new Image()
+      this.imageP2.src = images.edmundPlanet
+      // this.imageP2.onload = this.draw()
+      this.imageP22 = new Image()
+      this.imageP22.src = images.edmundPlanetFlip
+      this.imageP2.onload = this.draw()
+    }
+  
+    draw () {
+      ctxP2.drawImage(this.imageP2, this.x, this.y, this.width, this.height)    
+      ctxP2.drawImage(this.imageP22, this.x - this.width, this.y, this.width, this.height)
+      ctxP2.drawImage(this.imageP22, this.x + this.width, this.y, this.width, this.height)
+  
+      if (this.x < -canvasP2.width) {
+        this.x = 0
+      } 
+      
+      if (keys[65]) {
+        this.x++
+        
+      }
+      if (keys[68]) {
+        this.x--
+      }
+  
+    }
+  }
+
 class Farah {
   constructor () {
     this.x = 270
-    this.y = 220
-    this.width = 100
-    this.height = 100
+    this.y = 233
+    this.width = 90
+    this.height = 90
     this.image = new Image()
     this.image.src = images.farahWalk1
     this.image.onload = this.draw()
@@ -71,44 +176,11 @@ class Farah {
   }
 }
 
-class Planet1 {
-  constructor () {
-    this.x = 0
-    this.y = 0
-    this.width = canvasP1.width
-    this.height = canvasP1.height
-    this.image = new Image()
-    this.image.src = images.mannPlanet
-    // this.image.onload = this.draw()
-    this.image2 = new Image()
-    this.image2.src = images.mannPlanetFlip
-    this.image.onload = this.draw()
-  }
-
-  draw () {
-    ctxP1.drawImage(this.image, this.x, this.y, this.width, this.height)    
-    ctxP1.drawImage(this.image2, this.x - this.width, this.y, this.width, this.height)
-    ctxP1.drawImage(this.image2, this.x + this.width, this.y, this.width, this.height)
-
-    if (this.x < -canvasP1.width) {
-      this.x = 0
-    } 
-    
-    if (keys[65]) {
-      this.x++
-      
-    }
-    if (keys[68]) {
-      this.x--
-    }
-
-  }
-}
 
 class Tomasa {
   constructor () {
     this.x = 180
-    this.y = 220
+    this.y = 223
     this.width = 100
     this.height = 110
     this.image = new Image()
@@ -118,13 +190,13 @@ class Tomasa {
     this.velY = 2
     this.grounded = true
     this.jumping = false
-    this.jumpStrength = 2
+    this.jumpStrength = 9
     //horizontal
     this.velX = 0
   }
 
   draw() {
-    ctxP1.drawImage(this.image, this.x, this.y, this.width, this.height)
+    ctxP2.drawImage(this.image, this.x, this.y, this.width, this.height)
   }
 }
 
@@ -145,21 +217,6 @@ class Tomasa {
 //   }
 // }
 
-class Floor {
-  constructor () {
-    this.x = 0
-    this.y = 302
-    this.width = canvasP1.width
-    this.height = 50
-    this.image = new Image()
-    this.image.src = images.mannFloor
-    this.image.onload = this.draw()
-  }
-  
-    draw() {
-      ctxP1.drawImage(this.image, this.x, this.y, this.width, this.height)
-    }
-}
 
 // class Alien {
 //   constructor () {
@@ -190,13 +247,16 @@ class Floor {
     ctxP1.clearRect(0, 0, canvasP1.width, canvasP1.height)
     frames++
     planet1.draw()
+    floorP1.draw()
+    planet2.draw()
+    floorP2.draw()
     player1.draw()
-    floor.draw()
     // enemy.draw()
     drawTime()
     moveP1()
     // bisketo.draw()
     player2.draw()
+    moveP2()
   }
 
   function gameOver () {
@@ -225,13 +285,13 @@ function moveP1 () {
     player1.y += player1.velY
     player1.velY += gravityMann
   }
-  if(player1.y>floor.y){
+  if(player1.y>floorP1.y){
     player1.grounded = true
     player1.jumping = false
-    player1.y = floor.y - player1.height+20
+    player1.y = floorP1.y - player1.height+20
   }
   player1.x += player1.velX
-  player1.velX *= friction
+  player1.velX *= frictionMann
   //horizontal
 
   if(keys[68]){
@@ -251,45 +311,46 @@ function moveP1 () {
   }
 }
 
-// function moveP2 () {
-//   if(!player2.grounded){
-//     player2.y += player2.velY
-//     player2.velY += gravityMann
-//   }
-//   if(player2.y>floor.y){
-//     player2.grounded = true
-//     player2.jumping = false
-//     player2.y = floor.y - player2.height+20
-//   }
-//   player2.x += player2.velX
-//   player2.velX *= friction
-//   //horizontal
+function moveP2 () {
+  if(!player2.grounded){
+    player2.y += player2.velY
+    player2.velY += gravityEdmund
+  }
+  if(player2.y>floorP2.y){
+    player2.grounded = true
+    player2.jumping = false
+    player2.y = floorP2.y - player2.height+25
+  }
+  player2.x += player2.velX
+  player2.velX *= frictionEdmund
+  //horizontal
 
-//   if(keys[68]){
-//     player2.velX++
-//     // player2.velX++
-//   }
-//   if(keys[65]){
-//     player2.velX--
-//   }
-//   if(keys[87]){
-//     if(!player2.jumping){
-//       player2.velY = 0
-//       player2.grounded = false
-//       player2.jumping = true
-//       player2.velY += -player2.jumpStrength
+  if(keys[39]){
+    player2.velX++
+  }
+  if(keys[37]){
+    player2.velX--
+  }
+  if(keys[38]){
+    if(!player2.jumping){
+      player2.velY = 0
+      player2.grounded = false
+      player2.jumping = true
+      player2.velY += -player2.jumpStrength
       
-//     } 
-//   }
-// }
+    } 
+  }
+}
 
 
 // INSTANCES 
 
 let planet1 = new Planet1()
+let planet2 = new Planet2()
+let floorP1 = new FloorP1()
+let floorP2 = new FloorP2()
 let player1 = new Farah()
 // let enemy = new Alien()
-let floor = new Floor()
 // let bisketo = new Bisketo()
 let player2 = new Tomasa()
 
@@ -309,31 +370,31 @@ addEventListener('keydown', e=>{
 
 addEventListener('keydown', e => {
   if (e.keyCode === 87) {
-    document.getElementById('w-key').classList.toggle('key-press')
+    document.getElementById('w-key').classList.add('key-press')
   }
   if (e.keyCode === 68) {
-    document.getElementById("d-key").classList.toggle("key-press")
+    document.getElementById("d-key").classList.add("key-press")
   }
   if (e.keyCode === 65) {
-    document.getElementById('a-key').classList.toggle('key-press')
+    document.getElementById('a-key').classList.add('key-press')
   }
   if (e.keyCode === 83) {
-    document.getElementById('s-key').classList.toggle('key-press')
+    document.getElementById('s-key').classList.add('key-press')
   }
 })
 
 addEventListener('keyup', e => {
   if (e.keyCode === 87) {
-    document.getElementById('w-key').classList.toggle('key-press')
+    document.getElementById('w-key').classList.remove('key-press')
   }
   if (e.keyCode === 68) {
-    document.getElementById("d-key").classList.toggle("key-press")
+    document.getElementById("d-key").classList.remove("key-press")
   }
   if (e.keyCode === 65) {
-    document.getElementById('a-key').classList.toggle('key-press')
+    document.getElementById('a-key').classList.remove('key-press')
   }
   if (e.keyCode === 83) {
-    document.getElementById('s-key').classList.toggle('key-press')
+    document.getElementById('s-key').classList.remove('key-press')
   }
 })
 
